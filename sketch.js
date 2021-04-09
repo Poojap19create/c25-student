0eb4e48d7d53bc20b6bce50b573048fac19b8cf7
Poojap19create <74517001+Poojap19create@users.noreply.github.com>
2021-04-09 17:05:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -38,26 +38,3 @@ function setup(){
 
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
-
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
-
-    box5.display();
-    log4.display();
-    log5.display();
-platform.display()
-    bird.display();
-}
